2bb834626e890372d36069edd4d1293d19cfa194
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-12-20 09:48:46 +0600

    signup api integrate done

---
+++ lib/controllers/signup_controller.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todos_flutter_application/routes.dart';
+import 'package:todos_flutter_application/utils/baseurl.dart';
+import 'package:todos_flutter_application/utils/custom_snackbar.dart';
+import 'package:todos_flutter_application/widgets/loader.dart';
+import 'package:http/http.dart' as http;
+
+class SignupController extends GetxController {
+  late TextEditingController nameController,
+      contactController,
+      addressController,
+      emailController,
+      passwordController,
+      confirmPasswordController;
+
+  @override
+  void onInit() {
+    nameController = TextEditingController();
+    contactController = TextEditingController();
+    addressController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmPasswordController = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    contactController.dispose();
+    addressController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.onClose();
+  }
+
+  checkSignup() {
+    if (nameController.text.isEmpty) {
+      customSnackbar('Error', 'Name is required', 'error');
+    } else if (addressController.text.isEmpty) {
+      customSnackbar('Error', 'Address is required', 'error');
+    } else if (contactController.text.isEmpty) {
+      customSnackbar('Error', 'Contact is required', 'error');
+    } else if (emailController.text.isEmpty ||
+        GetUtils.isEmail(emailController.text) == false) {
+      customSnackbar('Error', 'A Valid email is required', 'error');
+    } else if (passwordController.text.isEmpty) {
+      customSnackbar('Error', 'Password is required', 'error');
+    } else if (passwordController.text != confirmPasswordController.text) {
+      customSnackbar('Error', 'Password doesnot match!', 'error');
+    } else {
+      Get.showOverlay(asyncFunction: () => signup(), loadingWidget: Loader());
+    }
+  }
+
+  signup() async {
+    var response = await http.post(Uri.parse(baseurl + 'signup.php'), body: {
+      'name': nameController.text,
+      'contact': contactController.text,
+      'address': addressController.text,
+      'email': emailController.text,
+      'password': passwordController.text,
+    });
+    var res = await json.decode(response.body); 
+    if (res['success']) {
+      customSnackbar('Success', res['message'], 'success');
+      Get.offAllNamed(Routes.login);
+    } else {
+      customSnackbar('Error', res['message'], 'error');
+    }
+  }
+}
--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todos_flutter_application/controllers/signup_controller.dart';
 
 import '../../routes.dart';
 import '../../widgets/custom_button.dart';
 import '../../widgets/custom_textfield.dart';
 
 class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+  SignupScreen({super.key});
+  final signupController = Get.put(SignupController());
 
   @override
   Widget build(BuildContext context) {
@@ -15,98 +17,108 @@ class SignupScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'My TODO',
-                  style: TextStyle(
-                    fontSize: 54,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
+          child: GetBuilder<SignupController>(builder: (controller) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const CustomTextField(
-                hint: 'Name',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const CustomTextField(
-                hint: 'Address',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const CustomTextField(
-                hint: 'Contact',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const CustomTextField(
-                hint: 'Email',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const CustomTextField(
-                hint: 'Password',
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const CustomTextField(
-                hint: 'Confirm Password',
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                label: 'Sign Up',
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text.rich(
-                TextSpan(
-                  style: const TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 20,
-                    color: Colors.grey,
-                  ),
-                  children: [
-                    const TextSpan(
-                      text: 'Already have an account? ',
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'My TODO',
+                    style: TextStyle(
+                      fontSize: 54,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
                     ),
-                    TextSpan(
-                      text: 'Login',
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Get.toNamed(Routes.login);
-                        },
-                      style: const TextStyle(
-                        color: Color(0xff6b7afc),
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ], 
+                  ),
                 ),
-              ),
-            ],
-          ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                  hint: 'Name',
+                  controller: controller.nameController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hint: 'Address',
+                  controller: controller.addressController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hint: 'Contact',
+                  controller: controller.contactController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hint: 'Email',
+                  controller: controller.emailController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hint: 'Password',
+                  obscureText: true,
+                  controller: controller.passwordController,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hint: 'Confirm Password',
+                  obscureText: true,
+                  controller: controller.confirmPasswordController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                  label: 'Sign Up',
+                  onPressed: () {
+                    controller.checkSignup();
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: 'Already have an account? ',
+                      ),
+                      TextSpan(
+                        text: 'Login',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Get.toNamed(Routes.login);
+                          },
+                        style: const TextStyle(
+                          color: Color(0xff6b7afc),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
         ),
       ),
     );
